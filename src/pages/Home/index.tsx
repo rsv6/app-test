c1af2e860b9915eaa1c9d8a0e133887793c1943f
Rafael S. Vieira <rafaelsvbr@gmail.com>
2025-03-26 11:34:52 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 const Home = () => {
     return (
         <div>
-            <h1>Welcome Home</h1>
+            <h1>Welcome Home RSV</h1>
             <Link to="/about">About</Link>
         </div>
         
